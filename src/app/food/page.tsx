5c565f8d1a662f9658288304563540e41fd0b903
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2023-05-20 10:49:50 +0600

    ok

--- src/app/food/page.tsx
+++ src/app/food/page.tsx
@@ -1,7 +1,8 @@
 "use client"
 
 import { data } from "@/Data/Data";
-import { motion, AnimatePresence } from "framer-motion"
+import FoodItem from "@/components/FoodItem";
+import { AnimatePresence } from "framer-motion"
 import { useState } from "react";
 
 const Food= () => {
@@ -56,22 +57,7 @@ const Food= () => {
             <AnimatePresence>
                 <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
                     {foods.map((item, index) => (
-                        <motion.div
-                            layout
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            exit={{ opacity: 0 }}
-                            transition={{ duration: 0.1 }}
-                            key={item.id}
-                            className="border shadow-lg rounded-lg hover:scale-105 duration-500 cursor-pointer">
-                            <img className='w-full h-[150px] md:h-[200px] object-cover rounded-t-lg' src={item.image} alt={item.name} />
-                            <div className='flex justify-between px-2 py-4'>
-                                <p>{item.name}</p>
-                                <p>
-                                    <span className='bg-orange-500 text-white p-1 rounded-md'>{item.price}</span>
-                                </p>
-                            </div>
-                        </motion.div>
+                        <FoodItem title={item?.name} price={item?.price} image={item?.image} key={item?.id}/>
                     ))}
                 </div>
             </AnimatePresence>
